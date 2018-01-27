ebae4b6a5893dc5708e628df3c23273220aafc3d
tate98 <tate.anderson@huskers.unl.edu>
2018-01-26 18:53:09 -0600

    changed modulus from 0 to 1

--- kaprekarUtils.c
+++ kaprekarUtils.c
@@ -25,19 +25,20 @@
 int isKaprekar(int n) {
 
   if(n < 1) {
-    return false;
+    return (0);
   }
 
   int i;
   long square = n * (long) n;
-  int numDigits = (int) log10(n) + 1;
-  long modulus = 0;
+  int numberOfDigits = (int) log10(n) + 1;
+  //changed modulus from 0 to 1
+  long modulus = 1;
   long first, second;
 
   //for each possible "split" of the square...
   for(i=1; i<=numberOfDigits; i++) {
     //increase the modulus by a factor of 10
-    modulous *= 10;
+    modulus *= 10;
 
     //split the square into two parts
     first = square / modulus;
